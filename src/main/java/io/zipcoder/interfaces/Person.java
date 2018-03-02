99f5a7b614a93c0c21a12d7e44e6f1c0892e6e0c
miscbits <willow6786@gmail.com>
2018-03-02 15:09:31 -0500

    made person class

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -1,5 +1,13 @@
 package io.zipcoder.interfaces;
 
 public class Person {
+    public final long id;
 
+    public Person(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
