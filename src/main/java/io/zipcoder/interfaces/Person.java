fb8a1538afd77d2ce69b98317a6782e54780d131
miscbits <willow6786@gmail.com>
2018-03-02 15:15:28 -0500

    added name and setters and getter for person

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -3,6 +3,16 @@ package io.zipcoder.interfaces;
 public class Person {
     public final long id;
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Person(long id) {
         this.id = id;
     }
@@ -10,4 +20,6 @@ public class Person {
     public long getId() {
         return id;
     }
+
+
 }
